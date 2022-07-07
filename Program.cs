25a89a977d9e7ed7edaea9e16936211dfbd9b5e6
sujesh <sujesh@EMTECH691>
2022-07-07 13:02:20 +0530

    new tst2

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace TestAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();//sachin
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
